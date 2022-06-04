63a3944d0dbbbbb1ac9e460a0b0b0e04fe05e934
st082196 <105283412+st082196@users.noreply.github.com>
2022-06-04 23:28:32 +0300

    Delete Оптимизация UCT потенциала directory

--- Оптимизация UCT потенциала/UCT_deviations.m
+++
@@ -1,6 +0,0 @@
-function f = UCT_deviations(parameter,table)
-%Вычисляет отклонения значений фаз потенциала UCT от значений из table
-
-UCT_table = UCT(parameter);
-UCT_table = UCT_table(1:size(table,1),1:size(table,2));
-f = abs(UCT_table-table);
--- Оптимизация UCT потенциала/potential.h
+++
@@ -1,7 +0,0 @@
-double* BONN_coupled(double parameter[17]);
-
-double* BONN_noncoupled(double parameter[17]);
-
-double* UCT_coupled(double parameter[17]);
-
-double* UCT_noncoupled(double parameter[17]);
